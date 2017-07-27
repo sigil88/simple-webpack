824997e32f297bcf300774eb78ea771e000ad66d
Jen Cant <jennifer.cant@gmail.com>
2017-07-28 00:58:03 +0100

    initial commit - basic working setup of vuejs and webpack with a webpack config file

---
+++ dist/index.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <title>Vue &amp; Webpack</title>
+  </head>
+  <body>
+  	<main id="app">
+  		<h1>{{ message }}</h1>
+  	</main>
+    <script src="app.js"></script>
+  </body>
+</html>
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+var path 	= require('path');
+var webpack = require('webpack')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+resolve: {
+  alias: {
+    'vue$': 'vue/dist/vue.js'
+  }
+}
+};
