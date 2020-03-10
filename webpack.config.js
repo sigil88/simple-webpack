9918b930e0cae41622423111fd919c93d1f5be57
Jen Cant <8853855+sigil88@users.noreply.github.com>
2020-03-10 20:59:37 +0000

    Updated to latest versions and started pulling in tailwindcss

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,19 @@
 var path 	= require('path');
-var webpack = require('webpack')
+var webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'app.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-resolve: {
-  alias: {
-    'vue$': 'vue/dist/vue.js'
-  }
-}
+	entry: './src/index.js',
+	output: {
+		filename: 'app.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	resolve: {
+		alias: {
+			'vue$': 'vue/dist/vue.js'
+		}
+	},
+	plugins: [
+		require('tailwindcss'),
+		require('autoprefixer'),
+	]
 };
